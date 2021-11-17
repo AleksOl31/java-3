20a9e36e38647e0409a97aef7b20073afb84b032
Oleg Shulyakov <43452558+AleksOl31@users.noreply.github.com>
2021-11-17 13:59:36 +0300

    lesson 2 home work

---
+++ ChatStarter.java
@@ -0,0 +1,9 @@
+package ru.alexanna.lesson_2;
+
+import ru.alexanna.lesson_2.adapter.ChatAdapter;
+
+public class ChatStarter {
+    public static void main(String[] args) {
+        new ChatAdapter();
+    }
+}
---
+++ server/DatabaseConnector.java
@@ -0,0 +1,31 @@
+package ru.alexanna.lesson_2.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DatabaseConnector {
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:sqlite:C:\\Users\\proc.LGOK\\Desktop\\sqlite-tools-win32-x86-3360000\\chat.db");
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong during getting a connection.", e);
+        }
+    }
+
+    public static void close(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong during closing a connection.", e);
+        }
+    }
+
+    public static void rollback(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong during a rollback operation.", e);
+        }
+    }
+}
