57f0c9668eb1a6920cfa066e2db08bc6198cc802
Oleg <procundo@yandex.ru>
2021-11-22 21:08:35 +0300

    lesson 3 home work

---
+++ ServerStarter.java
@@ -0,0 +1,9 @@
+package ru.alexanna.lesson_2;
+
+import ru.alexanna.lesson_2.server.ChatServer;
+
+public class ServerStarter {
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+}
